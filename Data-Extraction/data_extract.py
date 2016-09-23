0a08b7deb3cf5b2cb42f5a774ccd4a1315af52ba
JestonBlu <jestonblu@gmail.com>
2016-09-23 15:04:20 -0500

    updated python

--- Data-Extraction/data_extract.py
+++ Data-Extraction/data_extract.py
@@ -1,3 +1,12 @@
+#!/bin/python
+#
+# Python program for extracting data out of all of the excel files associated with this project
+#
+# This Program assumes its being exectuted from the parent directory and at that all of the excel
+# appear in the /Files directory.
+#
+# It will write a single csv file the parent directory (~750MB)
+
 import os
 import pandas as pd
 
@@ -8,14 +17,14 @@ lst = os.listdir("./Files")
 path = "./Files/"
 
 # First File
-output = pd.read_excel(path + lst[0], skiprows=8)
+output = pd.read_excel(path + lst[0], header=0, skiprows=8, parse_cols="A:J")
 
 # Create ID
 output["ID"] = lst[0]
 
 # Loop for all of the files in
-for x in lst[1:]:
-    tmp = pd.read_excel(path + x, header=1, skiprows=7)
+for x in lst[1:100]:
+    tmp = pd.read_excel(path + x, header=0, skiprows=8, parse_cols="A:J")
     tmp["ID"] = x
     output = output.append(tmp, ignore_index=True)
 
