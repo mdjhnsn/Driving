003a5e211d3b2ec3976a2339186cc84dac309d80
JestonBlu <jestonblu@gmail.com>
2016-09-26 13:59:41 -0500

    reorganized

---
+++ Files/extract_faces.py
@@ -0,0 +1,39 @@
+#!/bin/python
+#
+# Python program for extracting data out of all of the excel files associated with this project
+#
+# This Program assumes its being exectuted from the parent directory and at that all of the excel
+# appear in the /Files/Faces directory.
+#
+# It will write a single csv file the parent directory (~750MB)
+
+
+import os
+import pandas as pd
+
+# Get list of files in directory
+lst = sorted(os.listdir("./Files/Faces"))
+
+# Path to Files
+path = "./Files/Faces/"
+
+# Variable Start/Stop
+x1 = 0
+
+# First File
+output = pd.read_excel(path + lst[x1], header=8, parse_cols="A:J")
+
+# Create ID
+output["ID"] = lst[x1]
+
+# Loop for all of the files in
+for x in lst[x1 + 1:]:
+    tmp = pd.read_excel(path + x, header=8, parse_cols="A:J")
+    tmp["ID"] = x
+    output = output.append(tmp)
+
+# Create some ID columns
+output["ID"] = output["ID"].str[:8]
+
+# Export data
+output.to_csv("data.csv", index=False)
--- Files/extract_stimuli.py
+++ Files/extract_stimuli.py
@@ -3,31 +3,29 @@
 # Python program for extracting data out of all of the excel files associated with this project
 #
 # This Program assumes its being exectuted from the parent directory and at that all of the excel
-# appear in the /Files directory.
-#
-# It will write a single csv file the parent directory (~750MB)
+# appear in the /Files-Stimuli directory.
 
 import os
 import pandas as pd
 
 # Get list of files in directory
-lst = sorted(os.listdir("./Files", ))
+lst = sorted(os.listdir("./Files/Stimuli"))
 
 # Path to Files
-path = "./Files/"
+path = "./Files/Stimuli/"
 
 # Variable Start/Stop
 x1 = 0
 
 # First File
-output = pd.read_excel(path + lst[x1], header=8, parse_cols="A:J")
+output = pd.read_excel(path + lst[x1], header=8, parse_cols="A:G")
 
 # Create ID
 output["ID"] = lst[x1]
 
 # Loop for all of the files in
-for x in lst[x1+1:]:
-    tmp = pd.read_excel(path + x, header=8, parse_cols="A:J")
+for x in lst[x1 + 1:]:
+    tmp = pd.read_excel(path + x, header=8, parse_cols="A:G")
     tmp["ID"] = x
     output = output.append(tmp)
 
@@ -35,4 +33,4 @@ for x in lst[x1+1:]:
 output["ID"] = output["ID"].str[:8]
 
 # Export data
-output.to_csv("data.csv", index=False)
+output.to_csv("data-.csv", index=False)
