7138b64632bf04d7fc3f35dfbe741a42998a79af
mdjhnsn <mdjhnsn@gwu.edu>
2018-12-28 14:12:54 -0800

    add data files

--- Files/extract_faces.py
+++ Files/extract_faces.py
@@ -12,25 +12,35 @@ import os
 import pandas as pd
 
 # Get list of files in directory
-lst = sorted(os.listdir("Faces"))
+lst = sorted(os.listdir("Files/FACS Files"))
+lst.remove('.DS_Store')
 
 # Path to Files
-path = "Faces/"
+path = "Files/FACS Files/"
 
 # Variable Start/Stop
 x1 = 0
 
 # First File
-output = pd.read_excel(path + lst[x1], header=8, parse_cols="A:J")
+output = pd.read_excel(path + lst[x1], header=8, usecols="A:J")
 
 # Create ID
 output["ID"] = lst[x1]
 
 # Loop for all of the files in
 for x in lst[x1 + 1:]:
-    tmp = pd.read_excel(path + x, header=8, parse_cols="A:J")
-    tmp["ID"] = x
-    output = output.append(tmp)
+    if x['T034-005.xlsx']:
+        tmp = pd.read_excel(path + x, header=7, usecols="A:J", sort=False)
+        tmp["ID"] = x
+        output = output.append(tmp)
+    elif x['T009-006.xlsx']:
+        tmp = pd.read_excel(path + x, header=9, usecols="A:J", sort=False)
+        tmp["ID"] = x
+        output = output.append(tmp)
+    else:
+        tmp = pd.read_excel(path + x, header=8, usecols="A:J", sort=False)
+        tmp["ID"] = x
+        output = output.append(tmp)
 
 # Create some ID columns
 output["ID"] = output["ID"].str[:8]
