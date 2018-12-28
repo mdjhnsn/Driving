aa21be50870fa3f15e4b76cc75f2fa950b3507e5
mdjhnsn <mdjhnsn@gwu.edu>
2018-12-28 15:36:01 -0800

    extract stimuli

--- Files/extract_stimuli.py
+++ Files/extract_stimuli.py
@@ -2,33 +2,22 @@ import os
 import pandas as pd
 
 # Set local variables
-cwd = os.getcwd()
-lst = sorted(os.listdir(cwd + "/Files/Stimuli Files"))
-lst.remove('.DS_Store')  # Fixes problem with Mac OS X
-path_in = cwd + "/Files/Stimuli Files/"
-path_out = cwd + "/"
+lst = sorted(os.listdir("/Users/mj/Repos/Driving/Files/Stimuli Files"))
+path_in = "/Users/mj/Repos/Driving/Files/Stimuli Files/"
+path_out = "/Users/mj/Repos/Driving/Files/"
+
+# Fix problem with Mac OS X
+try:
+    lst.remove('.DS_Store')
+except Exception:
+    pass
 
 # Create empty data frame
 output = pd.DataFrame()
 
-# Get list of files in directory
-lst = sorted(os.listdir("Stimuli/"))
-
-# Path to Files
-path = "Stimuli/"
-
-# Variable Start/Stop
-x1 = 0
-
-# First File
-output = pd.read_excel(path + lst[x1], header=8, parse_cols="A,B,E,F,G")
-
-# Create ID
-output["ID"] = lst[x1]
-
-# Loop for all of the files in
-for x in lst[x1 + 1:]:
-    tmp = pd.read_excel(path + x, header=8, parse_cols="A,B,E,F,G")
+# Loop through files
+for x in lst[:3]:
+    tmp = pd.read_excel(path_in + x, header=8, parse_cols="A,B,E,F,G")
     tmp["ID"] = x
     output = output.append(tmp)
 
@@ -36,4 +25,4 @@ for x in lst[x1 + 1:]:
 output["ID"] = output["ID"].str[:8]
 
 # Export data
-output.to_csv("data-stimuli.csv", index=False)
+output.to_csv(path_out + "data-stimuli.csv", index=False)
