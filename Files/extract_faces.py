992091caffc04b0ac3f9233e24e85a29ceba1993
mdjhnsn <mdjhnsn@gwu.edu>
2018-12-31 00:30:32 -0800

    time to move on I think

--- Files/extract_faces.py
+++ Files/extract_faces.py
@@ -1,48 +1,31 @@
-# Load packages
 import os
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
-
-# Set local variables
-fls = sorted(os.listdir('/Users/mj/Repos/Driving/Files/FACS Files'))
-path_in = '/Users/mj/Repos/Driving/Files/FACS Files/'
-path_out = '/Users/mj/Repos/Driving/Files/'
-
-# Fixes problem with Mac OS X
+path_in = os.getcwd() + '/Files/FACS Files/'
+path_out = os.getcwd() + '/Files/'
+file_list = sorted(os.listdir(path_in))
 try:
-    fls.remove('.DS_Store')
+    file_list.remove('.DS_Store')
 except Exception:
     pass
 
-# Extract principal components
-pca = PCA(2)
 
-emotions = ['Anger', 'Contempt', 'DisguEt', 'Fear', 'Joy', 'Neutral', 'Sad', 'Surprise']
-
-# Create empty data frame
-output = pd.DataFrame()
-
-def get_header_col(file_name):
+def load_file(file_name):
     if file_name == 'T034-005.xlsx':
         header_col = 7
     elif file_name == 'T009-006.xlsx':
         header_col = 9
     else:
         header_col = 8
-    return header_col
-
-def load_file(path_in, header_col):
-    tmp = pd.read_excel(path_in, header=header_col, usecols='A:J', sort=False, index_col='Frame#')
-    tmp = tmp[emotions]
-    tmp_reduced = pd.DataFrame(pca.fit_transform(tmp))
-    tmp_reduced['ID'].str[:8] = path_in
-    output.append(tmp_reduced)
-    return output
+    df = pd.read_excel(path_in + file_name, header=header_col, usecols='A:J', sort=False, index_col='Frame#')
+    emotions = ['Anger', 'Contempt', 'Disgust', 'Fear', 'Joy', 'Neutral', 'Sad', 'Surprise']
+    df = df[emotions]
+    df['ID'] = file_name
+    return df
 
 
-# Create ID column
-output['ID'] = output['ID'].str[:8]
-
-# Export data
-output.to_csv(path_out + 'data-faces.csv', index=False)
+df = load_file(file_list[1])
+df.describe()
+pca = PCA(2)
+df_reduced = pd.DataFrame(pca.fit_transform(df))
